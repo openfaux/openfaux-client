bd646d6575d56d59fc5d07b769f3d7823ce8796a
mubaidr <mubaidr@gmail.com>
2013-12-29 13:57:13 +0500

    Update button click handler

--- opera/js/background.js
+++ opera/js/background.js
@@ -2,18 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var min = 1;
-var max = 2;
-var current = min;
-
-function updateIcon() {
-  chrome.browserAction.setIcon({path:"icon" + current + ".png"});
-  current++;
-
-  if (current > max)
-    current = min;
+function toggleState() {
+	localStorage.state = localStorage.state === 'on' ? 'off' : 'on';
+	renderIcon();
 }
-
-chrome.browserAction.onClicked.addListener(updateIcon);
-//chrome.browserAction.setBadgeText({text: "on"});
-updateIcon();
+function renderIcon() {
+	localStorage.state || (localStorage.state = 'on');
+	var state = '';
+	if (localStorage.state === 'off') {
+		state = '-gray';
+		chrome.browserAction.setBadgeText({
+			text: 'off'
+		});
+		chrome.browserAction.setBadgeBackgroundColor( {color: "#B3B3B3" });
+	} else {
+		chrome.browserAction.setBadgeText({
+			text: 'on'
+		});
+		chrome.browserAction.setBadgeBackgroundColor( {color: "#FFA200" });
+	}
+	chrome.browserAction.setIcon({
+		path: 'media/openfaux-icon-19px' + state + '.png'
+	});	
+}
+chrome.browserAction.onClicked.addListener(toggleState);
+renderIcon();
