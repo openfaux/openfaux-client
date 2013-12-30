89708a3e823297a682c100b437b5e58a25751ec7
Zlatan Vasović <legospace9876@gmail.com>
2013-12-30 11:58:43 +0100

    Drop redundant newlines

--- ie/OpenFaux/stdafx.h
+++ ie/OpenFaux/stdafx.h
@@ -30,4 +30,3 @@ using namespace ATL;
 #pragma comment(lib, "comsuppw.lib")
 #pragma comment(lib, "comctl32.lib")
 #pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
